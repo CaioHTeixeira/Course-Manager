a0929742ff192486ead0d1dce2f026042ad0fdb6
CaioHTeixeira <caioteixeiraaa@gmail.com>
2022-06-13 23:33:33 -0300

    course-manager angular

---
+++ src/app/star/star.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input } from "@angular/core";
+
+@Component({
+    selector: 'app-star',
+    templateUrl: './star.component.html',
+    styleUrls: ['./star.component.css'] 
+})
+export class StarComponent {
+
+    @Input()
+    rating: number = 0;
+
+    starWidth!: number;
+
+    ngOnChange(): void {
+        this.starWidth = this.rating * 74 / 5;
+    }
+}
