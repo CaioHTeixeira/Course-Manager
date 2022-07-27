8c51b6dfa8b3b4842f1dd55777793d6a7d2dc79c
CaioHTeixeira <caioteixeiraaa@gmail.com>
2022-07-27 17:37:58 -0300

    refactoring

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,28 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CourseService } from './courses/course.service';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { RouterModule } from '@angular/router';
-import { Error404Component } from './error-404/error-404.component';
 import { HttpClientModule } from '@angular/common/http';
 import { CourseModule } from './courses/course.module';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavBarComponent,
-    Error404Component,
+    AppComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     CourseModule,
+    CoreModule,
     RouterModule.forRoot([
       {
         path: '', redirectTo: 'courses', pathMatch: 'full'
-      },
-      {
-        path: '**', component: Error404Component
       }  
     ])
   ],
--- src/app/courses/course.module.ts
+++ src/app/courses/course.module.ts
@@ -2,23 +2,23 @@ import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { RouterModule } from "@angular/router";
-import { ReplacePipe } from "../pipe/replace.pipe";
-import { StarComponent } from "../star/star.component";
+import { StarModule } from "../shared/component/star/star.module";
 import { CourseAddComponent } from "./course-add.component";
 import { CourseInfoComponent } from "./course-info.component";
 import { CourseListComponent } from "./course-list.component";
+import { AppPipeModule } from "../shared/pipe/app-pipe.module";
 
 @NgModule({
     declarations: [
         CourseListComponent,
         CourseInfoComponent,
-        CourseAddComponent,
-        ReplacePipe,
-        StarComponent
+        CourseAddComponent
     ],
     imports: [
         FormsModule,
         CommonModule,
+        StarModule,
+        AppPipeModule,
         RouterModule.forChild([
             {
                 path: 'courses', component: CourseListComponent
